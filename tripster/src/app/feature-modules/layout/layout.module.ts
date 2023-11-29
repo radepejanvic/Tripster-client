903d45a7ab642f713149309ebc2fb27d8a7deefd
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-11-29 11:21:17 +0100

    feat: Added routing for home page

--- tripster/src/app/feature-modules/layout/layout.module.ts
+++ tripster/src/app/feature-modules/layout/layout.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ]
 })
 export class LayoutModule { }
