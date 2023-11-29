f0a2672c1fe0bbcda182556ffba0c148adc33eb1
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-11-29 15:03:52 +0100

    feat: Set HomeComponent as default route

--- tripster/src/app/infrastructure/routing/app-routing.module.ts
+++ tripster/src/app/infrastructure/routing/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../../feature-modules/layout/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },//default route
   {path: 'home', component: HomeComponent},
 ];
 
