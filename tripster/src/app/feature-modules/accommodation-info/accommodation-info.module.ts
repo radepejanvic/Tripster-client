99515bd21203f0eb8eb792c2aac64e09d06d8b81
Rade Pejanović <radepejanovic@gmail.com>
2023-11-29 02:22:04 +0100

    feat: Added components to accommodation-info module

--- tripster/src/app/feature-modules/accommodation-info/accommodation-info.module.ts
+++ tripster/src/app/feature-modules/accommodation-info/accommodation-info.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotosComponent } from './photos/photos.component';
 import { MaterialModule } from '../../infrastructure/material/material.module';
+import { RatingComponent } from './rating/rating.component';
+import { OverviewComponent } from './overview/overview.component';
+import { AmenityComponent } from './amenity/amenity.component';
 
 
 @NgModule({
   declarations: [
-    PhotosComponent
+    PhotosComponent,
+    RatingComponent,
+    OverviewComponent,
+    AmenityComponent
   ],
   imports: [
     CommonModule,
