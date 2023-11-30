6ab6cfb76d3c10dbbf4347beb5f67448fb3e93fa
Rade Pejanović <radepejanovic@gmail.com>
2023-11-30 15:59:28 +0100

    fix: Changed root path

--- tripster/src/app/infrastructure/routing/app-routing.module.ts
+++ tripster/src/app/infrastructure/routing/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from '../../feature-modules/layout/home/home.component'
 import { PhotosComponent } from '../../feature-modules/accommodation-info/photos/photos.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },//default route
+  { path: '', redirectTo: 'home', pathMatch: 'full' },//default route
   { path: 'home', component: HomeComponent },
   { path: 'accommodationInfo', component: PhotosComponent },
 ];
