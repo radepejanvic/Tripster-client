b0cbbfef3b87f747da71abdd94dc7c86a9e65923
Rade Pejanović <radepejanovic@gmail.com>
2023-12-07 22:20:23 +0100

    feat: Created getPhotos and binded photos

--- tripster/src/app/feature-modules/accommodation-info/accommodation-info.service.ts
+++ tripster/src/app/feature-modules/accommodation-info/accommodation-info.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Accommodation } from './model/accommodation.model';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +13,22 @@ export class AccommodationInfoService {
   getAccommodation(id: number): Observable<Accommodation> {
     return this.http.get<Accommodation>('http://localhost:8080/api/accommodations/' + id);
   }
+
+  getPhotos(id: number): Observable<string[]> {
+    return this.http.get<string[]>(`http://localhost:8080/api/photos/${id}`).pipe(
+      map((base64Strings: string[]) => {
+        const dataUrls = base64Strings.map(base64 => this.getDataUrl(base64));
+        console.log('Data URLs:', dataUrls);
+        return dataUrls;
+      })
+    );
+  }
+
+  private getDataUrl(base64String: string): string {
+    const contentType = 'image/jpeg';
+    return `data:${contentType};base64,${base64String}`;
+  }
+
+
 
 }
--- tripster/src/app/feature-modules/accommodation-info/photos/photos.component.ts
+++ tripster/src/app/feature-modules/accommodation-info/photos/photos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AccommodationInfoService } from '../accommodation-info.service';
 import { Accommodation } from '../model/accommodation.model';
 
@@ -9,7 +9,10 @@ import { Accommodation } from '../model/accommodation.model';
 })
 export class PhotosComponent implements OnInit {
 
+  @Input() id: number | undefined;
+
   accommodation!: Accommodation;
+  photos!: string[];
 
   constructor(private service: AccommodationInfoService) { }
 
@@ -20,9 +23,20 @@ export class PhotosComponent implements OnInit {
         console.log(result);
       },
       error: (err: any) => {
-        console.log(err);
+        console.error('Error fetching accommodation info', err);
       }
     });
+
+    this.service.getPhotos(12).subscribe({
+      next: (result: string[]) => {
+        this.photos = result;
+        console.log(result);
+      },
+      error: (err: any) => {
+        console.error('Error fetching photos', err);
+      }
+    })
+
   }
 
 }
