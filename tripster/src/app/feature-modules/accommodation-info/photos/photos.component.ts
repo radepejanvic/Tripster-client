8dee842f4e71d9ccde1f56cf9f06262be519571b
Rade Pejanović <radepejanovic@gmail.com>
2023-12-07 18:29:54 +0100

    feat: Binded accommodation from backend

--- tripster/src/app/feature-modules/accommodation-info/photos/photos.component.ts
+++ tripster/src/app/feature-modules/accommodation-info/photos/photos.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccommodationInfoService } from '../accommodation-info.service';
+import { Accommodation } from '../model/accommodation.model';
 
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
   styleUrl: './photos.component.css'
 })
-export class PhotosComponent {
+export class PhotosComponent implements OnInit {
+
+  accommodation!: Accommodation;
+
+  constructor(private service: AccommodationInfoService) { }
+
+  ngOnInit(): void {
+    this.service.getAccommodation(12).subscribe({
+      next: (result: Accommodation) => {
+        this.accommodation = result;
+        console.log(result);
+      },
+      error: (err: any) => {
+        console.log(err);
+      }
+    });
+  }
 
 }
