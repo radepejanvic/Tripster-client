17a7be26986e00d5ae2ab9292f605f2bf27dd6cf
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-09 18:26:44 +0100

    feat: Created basic-filter

---
+++ tripster/src/app/feature-modules/filter/basic-filter/basic-filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-filter',
+  templateUrl: './basic-filter.component.html',
+  styleUrl: './basic-filter.component.css'
+})
+export class BasicFilterComponent {
+
+}
--- tripster/src/app/feature-modules/filter/filter.module.ts
+++ tripster/src/app/feature-modules/filter/filter.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BasicFilterComponent } from './basic-filter/basic-filter.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicFilterComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    BasicFilterComponent
   ]
 })
 export class FilterModule { }
