ec785dad28c3be0df8c8d0ae26ae768aacbdb2e5
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-09 21:11:49 +0100

    feat: Created guest-rating-filter

--- tripster/src/app/feature-modules/filter/filter.module.ts
+++ tripster/src/app/feature-modules/filter/filter.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BasicFilterComponent } from './basic-filter/basic-filter.component';
 import { AdditionalFilterComponent } from './additional-filter/additional-filter.component';
+import { GuestRatingFilterComponent } from './guest-rating-filter/guest-rating-filter.component';
 
 
 
 @NgModule({
   declarations: [
     BasicFilterComponent,
-    AdditionalFilterComponent
+    AdditionalFilterComponent,
+    GuestRatingFilterComponent
   ],
   imports: [
     CommonModule
   ],
   exports:[
     BasicFilterComponent,
-    AdditionalFilterComponent
+    AdditionalFilterComponent,
+    GuestRatingFilterComponent
   ]
 })
 export class FilterModule { }
