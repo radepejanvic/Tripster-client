513c55d4b027bfa728ece6d023238dc762459d8d
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-09 21:30:25 +0100

    feat: Created time-filter

--- tripster/src/app/feature-modules/filter/filter.module.ts
+++ tripster/src/app/feature-modules/filter/filter.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { BasicFilterComponent } from './basic-filter/basic-filter.component';
 import { AdditionalFilterComponent } from './additional-filter/additional-filter.component';
 import { GuestRatingFilterComponent } from './guest-rating-filter/guest-rating-filter.component';
+import { ReservationStatusFilterComponent } from './reservation-status-filter/reservation-status-filter.component';
+import { TimeFilterComponent } from './time-filter/time-filter.component';
+import { UserTypeStatusFilterComponent } from './user-type-status-filter/user-type-status-filter.component';
 
 
 
@@ -10,7 +13,10 @@ import { GuestRatingFilterComponent } from './guest-rating-filter/guest-rating-f
   declarations: [
     BasicFilterComponent,
     AdditionalFilterComponent,
-    GuestRatingFilterComponent
+    GuestRatingFilterComponent,
+    ReservationStatusFilterComponent,
+    TimeFilterComponent,
+    UserTypeStatusFilterComponent
   ],
   imports: [
     CommonModule
@@ -18,7 +24,10 @@ import { GuestRatingFilterComponent } from './guest-rating-filter/guest-rating-f
   exports:[
     BasicFilterComponent,
     AdditionalFilterComponent,
-    GuestRatingFilterComponent
+    GuestRatingFilterComponent,
+    ReservationStatusFilterComponent,
+    TimeFilterComponent,
+    UserTypeStatusFilterComponent
   ]
 })
 export class FilterModule { }
