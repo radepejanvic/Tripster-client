3eaf52fc2bd89430ee7a8f14c19f5b8fced3a04a
Rade Pejanović <radepejanovic@gmail.com>
2023-12-10 23:28:38 +0100

    feat: Created new pricelist form

--- tripster/src/app/feature-modules/accommodation-info/price-list/price-list.component.ts
+++ tripster/src/app/feature-modules/accommodation-info/price-list/price-list.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class PriceListComponent {
 
+  start: Date = new Date();
+  end: Date = new Date();
+
+  priceLists: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  // formatDate(date: Date): string {
+  //   const options: Intl.DateTimeFormatOptions = { day: '2-digit', month: 'short', year: 'numeric' };
+  //   return date.toLocaleDateString('en-GB', options)
+  //     .replace(/(\d+) (\w+) (\d+)/, '$1. $2 $3');
+  // }
+
+  formatDate(date: Date): string {
+    const day = date.getDate();
+    const month = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
+    const year = date.getFullYear();
+    return `${day}. ${month} ${year}.`;
+  }
+
 }
