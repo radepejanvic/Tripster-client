21df33109a310e4889e13260b3c1755d1b3a43fd
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-12 22:23:23 +0100

    feat: Added additionalFilter

--- tripster/src/app/feature-modules/layout/filter-page/filter-page.component.ts
+++ tripster/src/app/feature-modules/layout/filter-page/filter-page.component.ts
@@ -11,6 +11,8 @@ import {
 	FormGroup,
 	Validators,
 } from '@angular/forms';
+import { HttpParams } from '@angular/common/http';
+import { Amenity } from 'src/app/shared/enum/amenity.enum';
 
 @Component({
 	selector: 'app-filter-page',
@@ -23,11 +25,6 @@ export class FilterPageComponent implements OnInit {
 	mainFormGroup: FormGroup;
 
 	ngOnInit(): void {
-		this.service
-			.getAccommodationByFiltersForGuest('dfgdfg')
-			.subscribe((value: AccommodationInfoCard[]) => {
-				this.accommodations = value;
-			});
 		// this.service
 		// 	.getAccommodationRequestByFiltersForAdmin('sdfdfdf')
 		// 	.subscribe((value: AccommodationRequest[]) => {
@@ -35,10 +32,16 @@ export class FilterPageComponent implements OnInit {
 		// 	});
 		this.mainFormGroup = this.fb.group({
 			basicFilter: this.fb.group({
-				destination: new FormControl(''),
+				destination: new FormControl(null),
 				checkIn: new FormControl('', Validators.required),
 				checkOut: new FormControl('', Validators.required),
 				numberOfGuest: new FormControl('', Validators.min(0)),
+			}),
+			additionalFilter: this.fb.group({
+				amenities: [],
+				type: [],
+				minPrice: new FormControl(''),
+				maxPrice: new FormControl(''),
 			}),
 		});
 	}
@@ -46,10 +49,30 @@ export class FilterPageComponent implements OnInit {
 	onBasicFilter(data: any) {
 		this.mainFormGroup.get('basicFilter')?.patchValue(data);
 	}
+	onAdditionalFilter(data: any) {
+		this.mainFormGroup.get('additionalFilter')?.patchValue(data);
+	}
 
 	sendRequest() {
 		if (this.mainFormGroup.get('basicFilter')?.valid) {
-			console.log(this.mainFormGroup.get('baseFilter')?.value);
+			const additionalFilter =
+				this.mainFormGroup.get('additionalFilter')?.value;
+			const basicFilter = this.mainFormGroup.get('basicFilter')?.value;
+			const params = new HttpParams()
+				.set('city', basicFilter.destination)
+				.set('start', new Date(basicFilter.checkIn).getTime())
+				.set('end', new Date(basicFilter.checkOut).getTime())
+				.set('numOfGuests', basicFilter.numberOfGuest)
+				.set('amenities', additionalFilter.amenities)
+				.set('minPrice', additionalFilter.minPrice)
+				.set('maxPrice', additionalFilter.maxPrice)
+				.set('type', additionalFilter.type);
+			console.log(params.toString());
+			this.service
+				.getAccommodationByFiltersForGuest(params)
+				.subscribe((value: AccommodationInfoCard[]) => {
+					this.accommodations = value;
+				});
 		}
 	}
 
