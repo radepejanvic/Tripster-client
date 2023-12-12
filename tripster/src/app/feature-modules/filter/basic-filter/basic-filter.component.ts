173faa7eaf19ecc29cd412236dc4ad03b093b4ae
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-12 12:32:49 +0100

    feat: Added binding

--- tripster/src/app/feature-modules/filter/basic-filter/basic-filter.component.ts
+++ tripster/src/app/feature-modules/filter/basic-filter/basic-filter.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-basic-filter',
-  templateUrl: './basic-filter.component.html',
-  styleUrl: './basic-filter.component.css'
+	selector: 'app-basic-filter',
+	templateUrl: './basic-filter.component.html',
+	styleUrl: './basic-filter.component.css',
 })
 export class BasicFilterComponent {
+	basicFilter = new FormGroup({
+		destination: new FormControl(''),
+		checkIn: new FormControl(''),
+		checkOut: new FormControl(''),
+		numberOfGuest: new FormControl(''),
+	});
 
+	@Output()
+	filterOneChange = new EventEmitter<any>();
+
+	onFilterChange() {
+		this.filterOneChange.emit(this.basicFilter.value);
+	}
 }
