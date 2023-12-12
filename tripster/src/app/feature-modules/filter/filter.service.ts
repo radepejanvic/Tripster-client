dbd69b56a8bb9f9f819b2595fed14c8323b45a31
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-12 17:50:52 +0100

    feat: Switched parametar any to HttpParams

--- tripster/src/app/feature-modules/filter/filter.service.ts
+++ tripster/src/app/feature-modules/filter/filter.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AccommodationInfoCard } from '../cards/accommodation-info-card/model/accommodation-info-card.model';
@@ -6,18 +6,25 @@ import { environment } from 'src/env/env';
 import { AccommodationRequest } from '../cards/accommodation-request-card/model/accommodation-request.mode';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class FilterService {
+	constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
+	getAccommodationByFiltersForGuest(
+		params: HttpParams
+	): Observable<AccommodationInfoCard[]> {
+		return this.http.get<AccommodationInfoCard[]>(
+			environment.apiHost + 'accommodations/guest/filters',
+			{ params }
+		);
+	}
 
-  getAccommodationByFiltersForGuest(some:any): Observable<AccommodationInfoCard[]>{
-    return this.http.get<AccommodationInfoCard[]>(environment.apiHost+"accommodations/guest")
-  }
-
-  getAccommodationRequestByFiltersForAdmin(some:any): Observable<AccommodationRequest[]>{
-    return this.http.get<AccommodationRequest[]>(environment.apiHost+"accommodations/admin")
-  }
-
+	getAccommodationRequestByFiltersForAdmin(
+		some: any
+	): Observable<AccommodationRequest[]> {
+		return this.http.get<AccommodationRequest[]>(
+			environment.apiHost + 'accommodations/admin'
+		);
+	}
 }
