cf3b01c343effae4533b818e60181eae20a416ea
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-13 15:28:14 +0100

    feat: Added validators for date

--- tripster/src/app/feature-modules/filter/basic-filter/basic-filter.component.ts
+++ tripster/src/app/feature-modules/filter/basic-filter/basic-filter.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+	AbstractControl,
+	FormControl,
+	FormGroup,
+	ValidationErrors,
+	Validators,
+} from '@angular/forms';
 
 @Component({
 	selector: 'app-basic-filter',
@@ -8,16 +14,53 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class BasicFilterComponent {
 	basicFilter = new FormGroup({
-		destination: new FormControl(''),
-		checkIn: new FormControl(''),
-		checkOut: new FormControl(''),
-		numberOfGuest: new FormControl(''),
+		destination: new FormControl(null),
+		checkIn: new FormControl(),
+		checkOut: new FormControl(),
+		numberOfGuest: new FormControl(0),
 	});
 
 	@Output()
 	filterChange = new EventEmitter<any>();
 
 	onFilterChange() {
-		this.filterChange.emit(this.basicFilter.value);
+		this.setCustomValidators();
+		if (this.basicFilter.valid) {
+			this.filterChange.emit(this.basicFilter.value);
+		}
+	}
+
+	setCustomValidators() {
+		this.basicFilter.setValidators(this.dateValidator.bind(this));
+		this.basicFilter.updateValueAndValidity();
+	}
+
+	dateValidator(control: AbstractControl): ValidationErrors | null {
+		var startDate = control.get('checkIn')?.value;
+		var endDate = control.get('checkOut')?.value;
+
+		if (startDate && endDate && startDate > endDate) {
+			this.basicFilter.get('checkIn')?.setValue(endDate);
+			startDate = endDate;
+			this.basicFilter.get('checkOut')?.setValue(null);
+			return null;
+		}
+
+		var today = new Date();
+		today.setDate(today.getDate() - 1);
+		if (startDate && new Date(startDate) < today) {
+			this.basicFilter.get('checkIn')?.setValue(null);
+			return null;
+		}
+		if (endDate && new Date(endDate) < today) {
+			this.basicFilter.get('checkOut')?.setValue(null);
+			return null;
+		}
+		if (new Date(startDate).getTime() === new Date(endDate).getTime()) {
+			this.basicFilter.get('checkOut')?.setValue(null);
+			return null;
+		}
+
+		return null;
 	}
 }
