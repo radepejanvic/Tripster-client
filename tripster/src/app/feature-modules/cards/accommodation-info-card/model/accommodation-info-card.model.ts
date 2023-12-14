61289c2f2897d2697e6fddf5b62b834c5b46aff8
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2023-12-14 16:31:15 +0100

    feat: Added pricePerNight

--- tripster/src/app/feature-modules/cards/accommodation-info-card/model/accommodation-info-card.model.ts
+++ tripster/src/app/feature-modules/cards/accommodation-info-card/model/accommodation-info-card.model.ts
@@ -1,15 +1,15 @@
-
-export interface AccommodationInfoCard{
-    id?: number,
-    name: string,
-    city: string,
-    street: string,
-    number: string,
-    shortDescription: string,
-    amenities: string[],
-    price:number,
-    numOfGuests: number,
-    duration: number,
-    rating: number,
-    numOfReviews:number;
+export interface AccommodationInfoCard {
+	id?: number;
+	name: string;
+	city: string;
+	street: string;
+	number: string;
+	shortDescription: string;
+	amenities: string[];
+	price: number;
+	pricePerNight: number;
+	numOfGuests: number;
+	duration: number;
+	rating: number;
+	numOfReviews: number;
 }
