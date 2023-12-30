750ec302a7e57d6b158d53b80323e40f91175866
Rade Pejanović <radepejanovic@gmail.com>
2023-12-30 16:53:05 +0100

    feat: Binded review form to backend

--- tripster/src/app/feature-modules/accommodation-info/review-form/review-form.component.ts
+++ tripster/src/app/feature-modules/accommodation-info/review-form/review-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Review } from '../model/accommodation.model';
+import { ReviewService } from '../review.service';
+import { AuthorizationService } from '../../authorization/authorization.service';
 
 @Component({
   selector: 'app-review-form',
@@ -7,6 +11,44 @@ import { Component, Input } from '@angular/core';
 })
 export class ReviewFormComponent {
   @Input() id!: number;
+
+  constructor(private reviewService: ReviewService, private authorizationService: AuthorizationService) { }
+
+  form = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    comment: new FormControl('', [Validators.required]),
+    rate: new FormControl(0, [Validators.required, Validators.min(1)]),
+  });
+
+  mapFormToReview(): Review {
+    const review: Review = {
+      title: this.form.value.title || "",
+      rate: this.form.value.rate || 0,
+      comment: this.form.value.comment || "",
+      reviewerId: this.authorizationService.getPersonId(),
+      reviewedId: this.id
+    }
+
+    return review;
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value.title, this.form.value.comment, this.form.value.rate);
+    if (!this.form.valid) {
+      console.error("Invalid form");
+    }
+
+    console.log(this.mapFormToReview());
+
+    this.reviewService.addReview(this.mapFormToReview()).subscribe({
+      next: (response: Review) => {
+        console.log(response);
+      },
+      error: (err: any) => {
+        console.error("Error posting new review.", err);
+      }
+    });
+  }
 
 
 }
