9f555e4862a6faee0fb3d3c6f1c672c226cd8b07
Rade Pejanović <radepejanovic@gmail.com>
2023-12-31 14:05:20 +0100

    feat: Binded review form pop-up to add review icon

--- tripster/src/app/feature-modules/accommodation-info/reviews/reviews.component.ts
+++ tripster/src/app/feature-modules/accommodation-info/reviews/reviews.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Review } from '../model/accommodation.model';
 import { AccommodationInfoService } from '../accommodation-info.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ReviewFormComponent } from '../review-form/review-form.component';
 
 @Component({
   selector: 'app-reviews',
@@ -11,7 +13,7 @@ export class ReviewsComponent implements OnInit {
   @Input() id!: number;
   reviews!: Review[];
 
-  constructor(private accommodationService: AccommodationInfoService) { }
+  constructor(private accommodationService: AccommodationInfoService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.accommodationService.getReviews(this.id).subscribe({
@@ -23,6 +25,16 @@ export class ReviewsComponent implements OnInit {
         console.error('Failed to load reviews.', err);
       }
     })
+  }
+
+  openReviewFormDialog(): void {
+    this.dialog.open(ReviewFormComponent, {
+      width: '400px',
+      data: {
+        id: this.id,
+        type: 'accommodation-review'
+      }
+    });
   }
 
 }
