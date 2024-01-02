a01244c20b43252b6b724045b988d018a35f62aa
Rade Pejanović <radepejanovic@gmail.com>
2024-01-02 12:01:31 +0100

    feat: Separated functions for host and accommodation reviews

--- tripster/src/app/feature-modules/accommodation-info/reviews/reviews.component.ts
+++ tripster/src/app/feature-modules/accommodation-info/reviews/reviews.component.ts
@@ -5,6 +5,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { ReviewFormComponent } from '../review-form/review-form.component';
 import { AuthorizationService } from '../../authorization/authorization.service';
 import { ReviewService } from '../review.service';
+import { InputModalityDetector } from '@angular/cdk/a11y';
+import { PersonUpdate } from '../../user-account-update/model/user-update.model';
 
 @Component({
   selector: 'app-reviews',
@@ -13,8 +15,10 @@ import { ReviewService } from '../review.service';
 })
 export class ReviewsComponent implements OnInit {
   @Input() id!: number;
+  @Input() host!: PersonUpdate;
   reviews!: Review[];
   reviewable: boolean = false;
+  checked: boolean = false;
 
   constructor(private accommodationService: AccommodationInfoService,
     private authorizationService: AuthorizationService,
@@ -22,24 +26,11 @@ export class ReviewsComponent implements OnInit {
     public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    this.accommodationService.getReviews(this.id).subscribe({
-      next: (response: Review[]) => {
-        this.reviews = response;
 
-      },
-      error: (err: any) => {
-        console.error('Failed to load reviews.', err);
-      }
-    })
+    // this.getAllAccommodationReviews();
+    // this.canReviewAccommodation();
 
-    this.reviewService.canReviewAccommodation(this.id, this.authorizationService.getPersonId()).subscribe({
-      next: (response: boolean) => {
-        this.reviewable = response;
-      },
-      error: (err: any) => {
-        console.error('Error checking accommodation reviewability.', err);
-      }
-    });
+    this.getAllHostReviews();
 
   }
 
@@ -52,5 +43,62 @@ export class ReviewsComponent implements OnInit {
       }
     });
   }
+
+  getAllAccommodationReviews(): void {
+    this.accommodationService.getReviews(this.id).subscribe({
+      next: (response: Review[]) => {
+        this.reviews = response;
+
+      },
+      error: (err: any) => {
+        console.error('Failed to load accommodation reviews.', err);
+      }
+    })
+  }
+
+  canReviewAccommodation(): void {
+    this.reviewService.canReviewAccommodation(this.id, this.authorizationService.getPersonId()).subscribe({
+      next: (response: boolean) => {
+        this.reviewable = response;
+      },
+      error: (err: any) => {
+        console.error('Error checking accommodation reviewability.', err);
+      }
+    });
+  }
+
+  getAllHostReviews(): void {
+    this.reviewService.getHostReviews(this.id).subscribe({
+      next: (response: Review[]) => {
+        this.reviews = response;
+
+      },
+      error: (err: any) => {
+        console.error('Failed to load host reviews.', err);
+      }
+    })
+  }
+
+  canReviewHost(): void {
+    this.reviewService.canReviewHost(this.host.id, this.authorizationService.getPersonId()).subscribe({
+      next: (response: boolean) => {
+        this.reviewable = response;
+      },
+      error: (err: any) => {
+        console.error('Error checking user reviewability.', err);
+      }
+    });
+  }
+
+  toggleReviews(): void {
+    if (this.checked) {
+
+    } else {
+      this.getAllAccommodationReviews();
+      this.canReviewAccommodation();
+    }
+
+  }
+
 
 }
