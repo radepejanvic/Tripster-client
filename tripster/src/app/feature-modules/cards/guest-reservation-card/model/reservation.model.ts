b332e172b751657cb87704045306ed7ceef16b7d
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-01-03 12:46:52 +0100

    feat: Created resrvation model

---
+++ tripster/src/app/feature-modules/cards/guest-reservation-card/model/reservation.model.ts
@@ -0,0 +1,10 @@
+export interface Reservation {
+  id?: number;
+  name: string;
+  address: string;
+  photo: string;
+  url?: string;
+  timeStamp: number;
+  duration: string;
+  status: string;
+}
