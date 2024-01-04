3543585b5ee000aba1f0fefb24efb2291fe50dbd
Rade Pejanović <radepejanovic@gmail.com>
2024-01-04 19:32:26 +0100

    feat: Created report service with report host, guest and getall

---
+++ tripster/src/app/feature-modules/report/report.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/env/env';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportService {
+
+  constructor(private http: HttpClient) { }
+
+  reportHost(report: Report): Observable<Report> {
+    return this.http.post<Report>(`${environment.apiHost}users/reports/host`, report);
+  }
+
+  reportGuest(report: Report): Observable<Report> {
+    return this.http.post<Report>(`${environment.apiHost}users/reports/guest`, report);
+  }
+
+  getAll(): Observable<Report[]> {
+    return this.http.get<Report[]>(`${environment.apiHost}users/reports`);
+  }
+
+}
