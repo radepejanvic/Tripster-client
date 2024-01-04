bea1590691647b5205209a72ee6f80e4dc788608
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-01-04 12:57:26 +0100

    feat: Sended transformation

--- tripster/src/app/feature-modules/filter/reservation-status-filter/reservation-status-filter.component.ts
+++ tripster/src/app/feature-modules/filter/reservation-status-filter/reservation-status-filter.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-status-filter',
   templateUrl: './reservation-status-filter.component.html',
-  styleUrl: './reservation-status-filter.component.css'
+  styleUrl: './reservation-status-filter.component.css',
 })
 export class ReservationStatusFilterComponent {
-  reservationStatus: string[] = ['Pending', 'Accepted', 'Cancelled','Denied'];
+  reservationStatus: string[] = [
+    'PENDING',
+    'ACCEPTED',
+    'CANCELLED',
+    'REJECTED',
+  ];
 
+  requestsFilter: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.requestsFilter = this.formBuilder.group({});
+
+    this.reservationStatus.forEach((item) =>
+      this.requestsFilter.addControl(item, this.formBuilder.control(false))
+    );
+  }
+
+  @Output()
+  filterChange = new EventEmitter<any>();
+
+  onFilterChange() {
+    this.filterChange.emit({
+      reservationType: this.getSelectedCheckboxes(),
+    });
+  }
+
+  getSelectedCheckboxes() {
+    const selectedCheckboxes = Object.keys(this.requestsFilter.value).filter(
+      (key) => this.requestsFilter.value[key]
+    );
+    return selectedCheckboxes;
+  }
 }
