c4c6c7726faa4afca31369e5707b0ac0ddebf2d1
Rade Pejanović <radepejanovic@gmail.com>
2024-01-04 19:31:41 +0100

    feat: Created report module

---
+++ tripster/src/app/feature-modules/report/report.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReportFormComponent } from './report-form/report-form.component';
+
+
+
+@NgModule({
+  declarations: [
+    ReportFormComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReportModule { }
