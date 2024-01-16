6d3e22d27a359b144fbeb85f1cae7c3c7f8b5f30
Nikola Trajkovic <nikolatrajkovicv@gmail.com>
2024-01-16 18:40:33 +0100

    feat: Added deleteReservation

--- tripster/src/app/feature-modules/cards/guest-reservation-card/guest-reservation-card.component.ts
+++ tripster/src/app/feature-modules/cards/guest-reservation-card/guest-reservation-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { AccommodationInfoCard } from '../accommodation-info-card/model/accommodation-info-card.model';
 import { Reservation } from './model/reservation.model';
+import { CardService } from '../card.service';
 
 @Component({
   selector: 'app-guest-reservation-card',
@@ -10,8 +11,20 @@ import { Reservation } from './model/reservation.model';
 export class GuestReservationCardComponent {
   @Input()
   reservation: Reservation;
+  constructor(private service: CardService) {}
 
   checkStatus(): boolean {
     return this.reservation.status == 'CANCELLED';
+  }
+
+  deleteReservation() {
+    this.service.deleteReservation(this.reservation.id || 0).subscribe({
+      next: (response: any) => {
+        window.location.reload();
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
   }
 }
